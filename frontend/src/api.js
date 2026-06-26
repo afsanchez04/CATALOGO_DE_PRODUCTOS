9fd9a54010acf17f7ffc915cf1be275938f18ea0
afsaanchez04 <afsanchez04@gmail.com>
2026-06-25 23:18:11 -0500

    fix baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,6 @@
 import axios from "axios"
 
 export const api = axios.create( { 
-  baseURL: "http://localhost:3000/api" ,
+  baseURL: "https://catalogo-de-productos-abgr.onrender.com/api" ,
   withCredentials: true
 } )
